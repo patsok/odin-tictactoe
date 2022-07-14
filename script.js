bf7d3f23838d20a4b264ca161825677e3e52c863
Falco <patryk.sokol@gmail.com>
2022-07-14 23:00:53 +0200

    add sign img & highlight winning position & fixes
    Fix(1) Wrong player winner attachment

--- script.js
+++ script.js
@@ -26,7 +26,11 @@ const gameController = (function() {
     const getUserChoice = (() => {
         const singleField = document.querySelectorAll('.single-field')
         singleField.forEach((field, i) => {
-            field.addEventListener('click', () => makeMove(i))
+            field.addEventListener('click', () => {
+                if (document.querySelector('.controls>p').textContent == ``) {
+                    makeMove(i, field)
+                }
+            })
         });
     })()
     const getActivePlayer = () => {
@@ -46,16 +50,16 @@ const gameController = (function() {
             patryk.setActivePlayer(true);
         }
     }
-    const makeMove = (i) => {
+    const makeMove = (i, field) => {
         document.querySelector('.controls>p').textContent = '';
         if (gameBoard.getGameBoardState()[i] == '') {
+            gameBoard.getGameBoardState()[i] = field.classList.add(`user-${getActivePlayer().getPlayerSign()}`);
             gameBoard.getGameBoardState()[i] = getActivePlayer().getPlayerSign();
             displayController.displayBoardState(gameBoard.getGameBoardState());
-            changeActivePlayer();
             if (gameController.checkWinCondition() || gameController.checkTie()) {
                 gameBoard.setGameBoardState();
-
             }
+            changeActivePlayer();
         }
     }
     const checkWinCondition = () => {
@@ -69,27 +73,53 @@ const gameController = (function() {
                 arr = [];
             }
         }
-        const checkRow = (board) => {
+
+        const checkRow = (board, pos) => {
             for (let i = 0; i < 3; i++) {
-                if (checkSingleRow(board[i])) {
+                if (checkSingleRow(board[i], i, pos)) {
                     return true;
                 }
             }
         }
-        const checkSingleRow = (row) => {
-            return row[0] == row[1] && row[0] == row[2] && row[0] != '';
+
+        const checkSingleRow = (row, i, pos) => {
+            if (row[0] == row[1] && row[0] == row[2] && row[0] != '') {
+                let winningFields;
+                if (pos == "rows") {
+                    winningFields = document.querySelectorAll(`.single-field.row${i+1}`);
+                }
+                if (pos == "cols") {
+                    winningFields = document.querySelectorAll(`.single-field.col${i+1}`);
+                }
+                winningFields.forEach(field => {
+                    field.classList.add(`win`)
+                });
+                return true;
+            }
         }
-        const checkDiagonal = (board) => {
-            return board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '';
+        const checkDiagonal = (board, pos) => {
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '') {
+                if (pos == "mainDiagonal") {
+                    for (let i = 0; i < 3; i++) {
+                        document.querySelector(`.single-field.col${i+1}.row${i+1}`).classList.add('win');
+                    }
+                }
+                if (pos == "secondaryDiagonal") {
+                    for (let i = 0; i < 3; i++) {
+                        document.querySelector(`.single-field.col${3-i}.row${i+1}`).classList.add('win');
+                    }
+                }
+                return true;
+            }
         }
 
         const performCheck = () => {
             const temporalTransposedGameBoardMatrix = gameBoardStateMatrix[0].map((col, i) => gameBoardStateMatrix.map(row => row[i]));
             const temporalReversedGameBoardMatrix = [...gameBoardStateMatrix].reverse();
-            if (checkRow(temporalTransposedGameBoardMatrix) ||
-                checkDiagonal(gameBoardStateMatrix) ||
-                checkDiagonal(temporalReversedGameBoardMatrix) ||
-                checkRow(gameBoardStateMatrix)) {
+            if (checkRow(temporalTransposedGameBoardMatrix, "cols") ||
+                checkDiagonal(gameBoardStateMatrix, "mainDiagonal") ||
+                checkDiagonal(temporalReversedGameBoardMatrix, "secondaryDiagonal") ||
+                checkRow(gameBoardStateMatrix, "rows")) {
                 return true;
             }
         }
@@ -97,28 +127,6 @@ const gameController = (function() {
             document.querySelector('.controls>p').textContent = `You won ${gameController.getActivePlayer().getPlayerName()}!`;
             return true;
         }
-
-        // for (let i = 0; i < gameBoardStateMatrix.length; i++) {
-        //     let game = gameBoardStateMatrix;
-        //     let winner = gameController.getActivePlayer().getPlayerName();
-        //     if (game[i][0] == game[i][1] && game[i][0] == game[i][2] && game[i][0] != '') {
-        //         result = true;
-        //     }
-        //     if (game[0][i] == game[1][i] && game[0][i] == game[2][i] && game[0][i] != '') {
-        //         result = true;
-        //     }
-        //     if (game[0][0] == game[1][1] && game[0][0] == game[2][2] && game[0][0] != '') {
-        //         result = true;
-        //     }
-        //     if (game[2][0] == game[1][1] && game[2][0] == game[0][2] && game[2][0] != '') {
-        //         result = true;
-        //     }
-        //     if (result == true) {
-        //         document.querySelector('.controls>p').textContent = `You won ${winner}!`;
-        //     }
-        // }
-        // return result;
-
     }
     const checkTie = () => {
         if (!gameBoard.getGameBoardState().includes('')) {
@@ -133,6 +141,13 @@ const gameController = (function() {
             displayController.displayBoardState(gameBoard.getGameBoardState());
             computer.setActivePlayer(false);
             patryk.setActivePlayer(true);
+            document.querySelector('.controls>p').textContent = ``;
+            const singleField = document.querySelectorAll('.single-field')
+            singleField.forEach(field => {
+                field.classList.remove(`user-O`);
+                field.classList.remove(`user-X`);
+                field.classList.remove(`win`);
+            });
         })
     })();
     return { checkWinCondition, getActivePlayer, checkTie };
@@ -145,6 +160,7 @@ const Player = (name) => {
         playerSign = sign;
     }
     const getPlayerSign = () => {
+
         return playerSign;
     }
     const setActivePlayer = (value) => {
