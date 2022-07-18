210970bb2d6716ad3cb3b2758f5da55e6596fb9d
Falco <patryk.sokol@gmail.com>
2022-07-18 22:05:15 +0200

    Add easy AI mode & fix bugs

--- script.js
+++ script.js
@@ -17,19 +17,23 @@ const gameBoard = (function() {
     const getGameBoardState = () => {
         return gameBoardState;
     }
-
-
     return { getGameBoardState, setGameBoardState };
 })()
 
 const gameController = (function() {
     const getUserChoice = (() => {
-        const singleField = document.querySelectorAll('.single-field')
+        const singleField = document.querySelectorAll('.single-field');
         singleField.forEach((field, i) => {
             field.addEventListener('click', () => {
                 const restartButtons = document.querySelectorAll('.controls button');
                 if (restartButtons[1].classList.contains('hidden')) {
-                    makeMove(i, field)
+                    let check = makePlayerMove(i, field);
+                    if (getActivePlayer().getPlayerName() == 'Computer' && !check) {
+                        singleField.forEach(field => {
+                            field.classList.add('no-click');
+                        })
+                        const timeout = setTimeout(makeComputerMove, 500);
+                    }
                 }
             })
         });
@@ -52,8 +56,16 @@ const gameController = (function() {
         }
         const restartButtons = document.querySelectorAll('.controls button');
         if (restartButtons[1].classList.contains('hidden')) {
-            document.querySelector('.controls>p').textContent = `Your turn ${getActivePlayer().getPlayerName()}`;
+            if (getActivePlayer().getPlayerName() == 'Computer') {
+                document.querySelector('.controls>p').textContent = `Computer's turn`;
+            } else {
+                document.querySelector('.controls>p').textContent = `Your turn ${getActivePlayer().getPlayerName()}`;
+            }
         }
+    }
+
+    const makePlayerMove = (i, field) => {
+        makeMove(i, field);
     }
     const makeMove = (i, field) => {
         document.querySelector('.controls>p').textContent = '';
@@ -75,16 +87,43 @@ const gameController = (function() {
                     const playersResults = document.querySelectorAll('.player-container p:nth-child(2n)');
                     playersResults[1].textContent = win;
                 }
+                return true;
             }
             if (gameController.checkTie()) {
                 gameBoard.setGameBoardState();
                 const restartButtons = document.querySelectorAll('.controls button');
                 restartButtons[0].classList.toggle('hidden');
                 restartButtons[1].classList.toggle('hidden');
+                return true;
             }
             changeActivePlayer();
         }
     }
+
+    const makeComputerMove = () => {
+        const difficulty = gameStartInterface.getDifficulty();
+        if (difficulty == 'easy') {
+            const singleField = document.querySelectorAll('.single-field')
+            const indexedEmptyBoard = gameBoard.getGameBoardState().map((empty, index) => {
+                if (empty == '') {
+                    return index;
+                }
+                return false;
+            }).filter(empty => typeof empty == "number");
+            const index = indexedEmptyBoard[Math.floor(Math.random() * indexedEmptyBoard.length)];
+            field = singleField[index];
+            makeMove(index, field)
+        } else if (difficulty == 'hard') {
+            console.log('hard')
+        } else {
+            console.log('impossible')
+        }
+        const singleField = document.querySelectorAll('.single-field')
+        singleField.forEach(field => {
+            field.classList.remove('no-click');
+        })
+    }
+
     const checkWinCondition = () => {
         let gameBoardStateMatrix = [];
         let arr = [];
@@ -157,22 +196,29 @@ const gameController = (function() {
             return true;
         }
     }
-    const restartGame = (() => {
+    const restartGame = () => {
+        gameBoard.setGameBoardState();
+        displayController.displayBoardState(gameBoard.getGameBoardState());
+        if (getActivePlayer().getPlayerName() == 'Computer') {
+            document.querySelector('.controls>p').textContent = `Computer's turn`;
+        } else {
+            document.querySelector('.controls>p').textContent = `Your turn ${getActivePlayer().getPlayerName()}`;
+        };
+        const singleField = document.querySelectorAll('.single-field');
+        singleField.forEach(field => {
+            field.classList.remove(`user-O`);
+            field.classList.remove(`user-X`);
+            field.classList.remove(`win`);
+        });
+        if (getActivePlayer().getPlayerName() == 'Computer') {
+            const timeout = setTimeout(makeComputerMove, 500);
+        }
+    };
+    const restartGameQuery = (() => {
         const restartButton = document.querySelectorAll('.controls button');
-        restartButton[0].addEventListener('click', () => {
-            gameBoard.setGameBoardState();
-            displayController.displayBoardState(gameBoard.getGameBoardState());
-            player2.setActivePlayer(false);
-            player1.setActivePlayer(true);
-            document.querySelector('.controls>p').textContent = ``;
-            const singleField = document.querySelectorAll('.single-field')
-            singleField.forEach(field => {
-                field.classList.remove(`user-O`);
-                field.classList.remove(`user-X`);
-                field.classList.remove(`win`);
-            });
-        })
+        restartButton[0].addEventListener('click', restartGame);
     })();
+
     const playNextGame = (() => {
         const restartButton = document.querySelectorAll('.controls button');
         restartButton[1].addEventListener('click', () => {
@@ -187,8 +233,15 @@ const gameController = (function() {
             });
             restartButton[0].classList.toggle('hidden');
             restartButton[1].classList.toggle('hidden');
-            document.querySelector('.controls>p').textContent = `Your turn ${getActivePlayer().getPlayerName()}`
-
+            if (getActivePlayer().getPlayerName() == 'Computer') {
+                document.querySelector('.controls>p').textContent = `Computer's turn`;
+            } else {
+                document.querySelector('.controls>p').textContent = `Your turn ${getActivePlayer().getPlayerName()}`;
+            }
+            changeActivePlayer();
+            if (getActivePlayer().getPlayerName() == 'Computer') {
+                setTimeout(makeComputerMove, 500);
+            }
         })
     })();
     return { checkWinCondition, getActivePlayer, checkTie, restartGame };
@@ -227,34 +280,73 @@ const Player = (name) => {
     return { getPlayerSign, setPlayerSign, setActivePlayer, getActivePlayer, getPlayerName, setPlayerName, addWin, setWins };
 }
 
-// const patryk = Player('Patryk');
-// const computer = Player('Morpheus');
-// patryk.setPlayerSign('X');
-// computer.setPlayerSign('O');
-// patryk.setActivePlayer(true);
-// computer.setActivePlayer(false);
-
-const player1 = Player('Temp1');
-const player2 = Player('Temp2');
+const player1 = Player('Player 1');
+const player2 = Player('Computer');
 
 const gameStartInterface = (function() {
-    const toggleGameVisibility = () => {
-        document.querySelector('.game-container').classList.remove('hidden');
+    const startButton = document.querySelectorAll('.header button')
+    const form = document.querySelector('form');
+    let difficulty = '';
+
+    const hideElement = (element) => {
+        if (typeof element == "string") {
+            document.querySelector(element).classList.add('hidden');
+        } else {
+            element.classList.add('hidden');
+        }
     }
-    const toggleFormVisibility = () => {
-        document.querySelector('form').classList.toggle('hidden');
+    const showElement = (element) => {
+        if (typeof element == "string") {
+            document.querySelector(element).classList.remove('hidden');
+        } else {
+            element.classList.remove('hidden');
+        }
+    }
+    const toggleElement = (element) => {
+        if (typeof element == "string") {
+            document.querySelector(element).classList.toggle('hidden');
+        } else {
+            element.classList.toggle('hidden');
+        }
+    }
+    const toggleButtonVisibility = () => {
+        let buttons = document.querySelectorAll('.mode-buttons button');
+        buttons.forEach(button => {
+            toggleElement(button)
+        });
     }
     const startPlayersGame = () => {
-        toggleFormVisibility();
+        showElement('form');
+        const labels = document.querySelectorAll('label');
+        const inputFields = document.querySelectorAll('.input-field');
+        const inputs = inputFields[1].querySelector('input');
+        showElement(labels[0]);
+        hideElement(labels[1]);
+        showElement(inputFields[1]);
+        hideElement(inputFields[2]);
+        showElement(inputFields[3]);
+        inputs.value = '';
+        inputs.disabled = false;
     }
     const startAIGame = () => {
-
+        showElement('form');
+        const labels = document.querySelectorAll('label');
+        const inputFields = document.querySelectorAll('.input-field');
+        const inputs = inputFields[1].querySelector('input');
+        hideElement(labels[0]);
+        showElement(labels[1]);
+        hideElement(inputFields[1]);
+        hideElement(inputFields[3]);
+        showElement(inputFields[2]);
+        inputs.value = 'Computer';
+        inputs.disabled = true;
     }
     const startGame = (e) => {
         if (form.checkValidity() == true) {
             e.preventDefault();
-            toggleGameVisibility();
-            toggleFormVisibility();
+            toggleElement('.game-container');
+            toggleElement('form');
+            toggleButtonVisibility();
             setPlayers();
             player1.setWins();
             player2.setWins();
@@ -262,16 +354,32 @@ const gameStartInterface = (function() {
             playersResults[0].textContent = 0;
             playersResults[1].textContent = 0;
             const restartButton = document.querySelectorAll('.controls button');
-            restartButton[0].classList.remove('hidden');
-            restartButton[1].classList.add('hidden');
-            // gameController.restartGame();
+            showElement(restartButton[0]);
+            hideElement(restartButton[1]);
+            gameController.restartGame();
         }
     }
-    const startButton = document.querySelectorAll('.header button')
+
+    const setInterface = () => {
+        hideElement('.game-container');
+        toggleButtonVisibility();
+
+        startButton[0].disabled = false;
+        startButton[1].disabled = false;
+    }
+
     startButton[0].addEventListener('click', startAIGame);
     startButton[1].addEventListener('click', startPlayersGame);
-    const form = document.querySelector('form');
+    startButton[2].addEventListener('click', setInterface);
     form.addEventListener('submit', e => startGame(e));
+    form.addEventListener('submit', e => setDifficulty(e));
+
+    const setDifficulty = (e) => {
+        difficulty = e.submitter.textContent.toLowerCase();
+    }
+    const getDifficulty = () => {
+        return difficulty;
+    }
 
     const setPlayers = () => {
         const playersNames = document.querySelectorAll('.player-container p');
@@ -284,6 +392,7 @@ const gameStartInterface = (function() {
         playersNames[0].textContent = player1.getPlayerName();
         playersNames[2].textContent = player2.getPlayerName();
         document.querySelector('.controls>p').textContent = `Your turn ${gameController.getActivePlayer().getPlayerName()}`
-
     }
+
+    return { getDifficulty };
 })()
